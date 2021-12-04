102bd74f60c25c1e28bc8070e9f9d6994c421b7a
tiagorahal <rahal.aires@gmail.com>
2021-12-04 06:44:05 -0300

    Update README

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ApplicationCable::ConnectionTest < ActionCable::Connection::Test
+class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
   #   cookies.signed[:user_id] = 42
   #
