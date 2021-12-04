29363c84d917643e256c8f5a04af23ca5a76a679
tiagorahal <rahal.aires@gmail.com>
2021-12-04 06:42:47 -0300

    Finish form to create user, create post methods, button to like a post

--- db/migrate/20211118221648_create_posts.rb
+++ db/migrate/20211118221648_create_posts.rb
@@ -1,12 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.references :users, null: false, foreign_key: true, index: true
-      t.string :title, limit: 10
+      t.references :user, null: false, foreign_key: true, index: true
+      t.string :title
       t.text :text
       t.timestamps
-      t.integer :comments_counter, default: 0, null: false
-      t.integer :likes_counter, default: 0, null: false
+      t.integer :comments_count, default: 0, null: false
+      t.integer :likes_count, default: 0, null: false
     end
   end
 end
