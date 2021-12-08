d4621134e46ed658f29966fa3f39fd128f1402b8
Erick Contreras <eri89--@hotmail.com>
2021-12-08 09:37:34 -0600

    add comfiramable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :photo, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :bio, :photo, :password, :current_password)
+    end
   end
 end
